c6df181550687bd57ca46b0eed83c93ee41229b4
Luis Enrique Jimenez <42558591+Don-kikin@users.noreply.github.com>
2026-09-21 20:52:31 -0600

    Delete hello.js

--- hello.js
+++
@@ -1,7 +0,0 @@
-
-let nombre = "Luis enrique"
-console.log("Hello world, " + nombre + "!");
-console.log("Soy Kike");
-console.log("Estoy aprendiendo JAVA");
-
-console.log("Ejecutando rama login");
